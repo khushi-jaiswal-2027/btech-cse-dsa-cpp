6e7566e8298b1edf7a4d33c0981da7414260d599
khushi-jaiswal-2027 <khushiji929@gmail.com>
2026-07-31 09:50:55 +0530

    Add standard right-angled star triangle pattern generator

---
+++ 10-star-triangle.cpp
@@ -0,0 +1,24 @@
+/*
+============================================================================
+QUESTION: Print star triangle of a given  number.
+LOGIC: Using two "for loops " where  loop-1 controls rows and loop-2 controls
+columns.
+----------------------------------------------------------------------------
+*/
+#include<iostream>
+using namespace std;
+int main(){ 
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+/*outer loop-1 it controls rows of triangle grid in which variable 
+    'i' declared as an integer and initial point from 1. */
+       for(int j=1;j<=i;j++){
+/*inner loop-2 it controls  columns up to ongoing  row, and  in which variable
+    'j' declared as an integer and initial point from 1. */
+        cout<<"* ";//Printing of star(*) with space. 
+       }cout<<endl;//Cleanly moves cursor to the next line after completing a row.
+    }
+    return 0;
+}
