03026045d916a0d3d37c66dc4873a81bd3dbe272
khushi-jaiswal-2027 <khushiji929@gmail.com>
2026-09-13 15:09:31 +0530

    Adding  star rhombus program

--- 19-star-rhombus.cpp
+++ 19-star-rhombus.cpp
@@ -34,7 +34,7 @@ int main() {
             cout<<"* ";
         } 
         cout << endl; 
-// Move to the next line after completing the row.
+// Move to next line after completing the row.
     }
     return 0;
 }
